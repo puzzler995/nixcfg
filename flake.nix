7adc21659e48bdd9f7302771300aa16f0e909af6
Katherine Marsee <ksmarsee@gmail.com>
2025-06-15 00:50:07 -0400

    home manager

--- flake.nix
+++ flake.nix
@@ -3,18 +3,36 @@
   
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+    #home-manager
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     
     #Helix Editor
     helix.url = "github:helix-editor/helix/master";
   };
   
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./configuration.nix
-      ];
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+
+              users.kat = import ./home.nix;
+            };
+          }
+        ];
+      };
     };
   };
 } 
---
+++ home.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "kat";
+  home.homeDirectory = "/home/kat";
+
+  home.packages = with pkgs; [
+    neofetch
+    jq
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "Katherine Marsee";
+    userEmail = "ksmarsee@gmail.com";
+  };
+
+  home.stateVersion = "25.05";
+}
