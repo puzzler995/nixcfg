2002eaee9fb3816b2d7cfc7f5920e832993b9d4b
Katherine Marsee <ksmarsee@gmail.com>
2025-06-15 21:48:45 -0400

    SHIT DONT WORK

--- hosts/nixos-test/default.nix
+++ hosts/nixos-test/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, self, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   imports =
@@ -79,7 +79,7 @@
     git
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-    self.inputs.helix.packages."${pkgs.system}".helix
+    # self.inputs.helix.packages."${pkgs.system}".helix
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
