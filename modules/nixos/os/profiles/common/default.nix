08853da8188f760a890dee828b148c13c078437c
Katherine Marsee <ksmarsee@gmail.com>
2025-06-23 18:17:13 -0400

    does this fix the stupid flake lock issue?

--- modules/nixos/os/profiles/common/default.nix
+++ modules/nixos/os/profiles/common/default.nix
@@ -20,8 +20,8 @@
       ];
 
       variables = {
-        FLAKE = lib.mkDefault "git+https://github.com/puzzler995/nixcfg.git";
-        NH_FLAKE = lib.mkDefault "git+https://github.com/puzzler995/nixcfg.git";
+        FLAKE = lib.mkDefault "git+ssh://git@github.com/puzzler995/nixcfg.git";
+        NH_FLAKE = lib.mkDefault "git+ssh://git@github.com/puzzler995/nixcfg.git";
       };
     };
 
