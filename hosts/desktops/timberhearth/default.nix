06bbcd36af762096dd964f7849ef39a35d84eed5
Katherine Marsee <ksmarsee@gmail.com>
2025-06-22 20:11:42 -0400

    lol desktop time?

--- hosts/desktops/timberhearth/default.nix
+++ hosts/desktops/timberhearth/default.nix
@@ -13,7 +13,7 @@
     self.nixosModules.locale-en-us
   ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "uas" "usbhid" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "uas" "usb_storage" "usbhid" "sd_mod" ];
 
   
 }
