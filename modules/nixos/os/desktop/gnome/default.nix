7d0a6c3787672ddc46ab22a8fd150533b3ea5249
Katherine Marsee <ksmarsee@gmail.com>
2025-06-23 20:52:21 -0400

    borf?

--- modules/nixos/os/desktop/gnome/default.nix
+++ modules/nixos/os/desktop/gnome/default.nix
@@ -2,8 +2,8 @@
   options.nixOSManager.desktop.gnome.enable = lib.mkEnableOption "enable GNOME";
 
   config = lib.mkIf config.nixOSManager.desktop.gnome.enable {
-    services.desktopManager.gnome.enable = true;
-    services.displayManager.gdm.enable = true; #TODO: MOVE
+    services.xserver.desktopManager.gnome.enable = true;
+    services.xserver.displayManager.gdm.enable = true; #TODO: MOVE
     nixOSManager.desktop.enable = true;
   };
 }
