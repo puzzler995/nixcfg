b59bd89647671cfa52763b952b77dbffdef0a060
Katherine Marsee <ksmarsee@gmail.com>
2025-06-23 21:23:54 -0400

    saving would help

--- modules/nixos/os/programs/steam/default.nix
+++ modules/nixos/os/programs/steam/default.nix
@@ -4,5 +4,31 @@
   pkgs,
   ...
 }: {
-  options.myNixOs
+  options.nixOSManager.programs.steam = {
+    enable = lib.mkEnableOption "Steam";
+    session.enable = lib.mkEnableOption "Steam + Gamescope";
+  };
+
+  config = lib.mkIf config.nixOSManager.programs.steam.enable {
+    environment.sessionVariables = {
+      STEAM_EXTRA_COMPAT_TOOLS_PATHS = lib.makeSearchPathOutput "steamcompattool" "" config.programs.steam.extraCompatPackages;
+    };
+
+    hardware.steam-hardware.enable = true;
+
+    programs = {
+      gamescope.enable = true;
+
+      steam = {
+        enable = true;
+        dedicatedServer.openFirewall = true;
+        extest.enable = true;
+        extraCompatPackages = with pkgs; [proton-ge-bin];
+        gamescopeSession.enable = config.nixOSManager.programs.steam.session.enable;
+        localNetworkGameTransfers.openFirewall = true;
+        protontricks.enable = true;
+        remotePlay.openFirewall = true;
+      };
+    };
+  };
 }
