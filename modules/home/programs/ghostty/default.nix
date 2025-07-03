2a51bc39b0a54117b6192705a4c2d5dc9ae0dd20
Katherine Marsee <ksmarsee@gmail.com>
2025-07-03 16:58:02 -0400

    ghostty config changes

--- modules/home/programs/ghostty/default.nix
+++ modules/home/programs/ghostty/default.nix
@@ -5,6 +5,8 @@
       enable = true;
       package = lib.mkIf pkgs.stdenv.isDarwin null;
       settings = {
+        app-notifications = "no-clipboard-copy";
+        copy-on-select = "clipboard";
         gtk-single-instance = lib.mkIf pkgs.stdenv.isLinux true;
         quit-after-last-window-closed = lib.mkIf pkgs.stdenv.isLinux false;
       };
