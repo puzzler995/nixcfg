20644ba9f092d465f1471dcb6d010b9a7d27d473
Katherine Marsee <ksmarsee@gmail.com>
2025-07-02 22:22:25 -0400

    add linux builder

--- modules/darwin/programs/nix/default.nix
+++ modules/darwin/programs/nix/default.nix
@@ -13,7 +13,23 @@
         interval = [{Hour = 12;}];
       };
 
+      linux-builder = {
+        enable = true;
+    ephemeral = true;
+    maxJobs = 4;
+    config = {
+      virtualisation = {
+        darwin-builder = {
+          diskSize = 40 * 1024;
+          memorySize = 8 * 1024;
+        };
+        cores = 6;
+      };
+    };
+      };
+
       settings = {
+        trusted-users = ["@admin"];
         experimental-features = ["nix-command" "flakes"];
       };
 
