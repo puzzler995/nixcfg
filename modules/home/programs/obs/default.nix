e74e868e70dd9169b12b290fa3d3e00a996886ea
Katherine Marsee <ksmarsee@gmail.com>
2025-07-18 15:41:57 -0400

    add nvenc to OBS

--- modules/home/programs/obs/default.nix
+++ modules/home/programs/obs/default.nix
@@ -9,7 +9,9 @@
   config = lib.mkIf config.homeManager.programs.obs.enable {
     programs.obs-studio = {
       enable = true;
-      package = pkgs.obs-studio;
+      package = (pkgs.obs-studio.override {
+        cudaSupport = true;
+      });
       plugins = [
         pkgs.obs-studio-plugins.obs-pipewire-audio-capture
         # pkgs.obs-studio-plugins.distroav
