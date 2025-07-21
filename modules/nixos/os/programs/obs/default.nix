980d7ff55ddbf80536586569da5d4585387acb92
Katherine Marsee <ksmarsee@gmail.com>
2025-07-21 19:30:19 -0400

    ?

--- modules/nixos/os/programs/obs/default.nix
+++ modules/nixos/os/programs/obs/default.nix
@@ -11,9 +11,9 @@
   };
 
   config = lib.mkIf config.nixOSManager.programs.obs.enable {
-    environment.systemPackages = with self.inputs.katpkgs.packages.${system}; [
-        nightbot-now-playing
-        touch-portal
+    environment.systemPackages = with self.inputs.katpkgs.packages; [
+        ${system}.nightbot-now-playing
+        ${system}.touch-portal
     ];
     programs.obs-studio = {
       enable = true;
