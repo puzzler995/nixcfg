6f4cdd64d81ae72401c427ececfa57d19b29063d
Katherine Marsee <ksmarsee@gmail.com>
2025-07-23 21:35:23 -0400

    sddm module

--- modules/nixos/os/services/default.nix
+++ modules/nixos/os/services/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ./tailscale
     ./gdm
+    ./sddm
   ];
 }
---
+++ modules/nixos/os/services/sddm/default.nix
@@ -0,0 +1,30 @@
+{config, lib, ...}: {
+    options.nixOSManager.services.sddm = {
+        enable = lib.mkEnableOption "sddm display manager";
+
+        autoLogin = lib.mkOption {
+            description = "User to autologin.";
+            default = null;
+            type = lib.types.nullOr lib.types.str;
+        };
+    };
+
+    config = lib.mkIf config.nixOSManager.services.sddm.enable {
+        security.pam.services.sddm = {
+            enableGnomeKeyring = true;
+            gnupg.enable = true;
+            kwallet.enable = true;
+        };
+
+        services = {
+            displayManager = {
+                autoLogin = lib.mkIf (config.nixOSManager.services.sddm.autoLogin != null) {
+                    enable = true;
+                    user = config.nixOSManager.services.sddm.autoLogin;
+                };
+
+                sddm.enable = true;
+            };
+        };
+    };
+}
